2ed3d6662f39a5810adf4ac0d92399ba3309e445
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-06 23:52:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function scuberGreetingForFeet(number) {
   if (number <= 400) {
     result = "This one is on me!";
   }
-  else if (2000 < number) {
+  else if (number > 2000) {
     result = "I will gladly take your thirty bucks."
   }
   else if (number >= 2500) {
