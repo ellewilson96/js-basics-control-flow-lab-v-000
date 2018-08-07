611ec781a440a5d451ff72f3442236ed824da164
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-07 00:00:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 // Write your code in this file!
 function scuberGreetingForFeet(number) {
-
-let result
   if (number <= 400) {
-    result = "This one is on me!";
+    return = "This one is on me!";
   }
   else if (number > 2000 && number < 2500) {
-    result = "I will gladly take your thirty bucks."
+    return = "I will gladly take your thirty bucks."
   }
-  else if (number >= 2500) {
-    result = "No can do."
+  else {
+    return = "No can do."
   }
   return result
 }
